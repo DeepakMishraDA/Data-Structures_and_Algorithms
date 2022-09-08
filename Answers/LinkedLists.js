386bf82b34fc4cbfc2da418cb2124d7dbfbd7e3c
deepak mishra <d.mishra@green-fusion.de>
2022-09-09 01:11:35 +0200

    Linked list

---
+++ Answers/LinkedLists.js
@@ -0,0 +1,17 @@
+class Node{
+    constructor(data,next = null){
+        this.data = data
+        this.next = next    
+    }
+}
+
+class LinkedList {
+    constructor(){
+       
+    }
+}
+const firstNode = new Node(1000)
+
+
+
+console.log(firstNode.data);
